d6f7218a3c5d50f2c3e9bbd9329864b423a48133
soojubm <30281891+soojubm@users.noreply.github.com>
2019-11-09 17:29:43 +0900

    상품 상페 페이지 상세 정보 샘플 유아이

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -46,38 +46,64 @@ const router = () => {
 					});
 				}
 				
-				const carousel = () => {};
-
-				// const category = document.querySelector('.slider');
-				// const categoryList = category.querySelector('.slider-viewer');
-				// const categoryListSlide = category.querySelectorAll('.slider-viewer > button');
-				// const categoryNavigationPrev = category.querySelector('.slider-arrows-prev');
-				// const categoryNavigationNext = category.querySelector('.slider-arrows-next');
-				// const categoryDots = category.querySelectorAll('.slider-dots-dot');
-
-				// const categoryContainerWidth = categoryList.offsetWidth;
-				// let sum = 0;
+				const carousel = () => {
+					const category = document.querySelector('.slider');
+					if(!category) return;
+					const categoryList = category.querySelector('.slider-viewer');
+					const categoryListSlide = category.querySelectorAll('.slider-viewer > button');
+					const categoryNavigationPrev = category.querySelector('.slider-arrows-prev');
+					const categoryNavigationNext = category.querySelector('.slider-arrows-next');
+					const categoryDots = category.querySelectorAll('.slider-dots-dot');
+	
+					const categoryContainerWidth = categoryList.offsetWidth;
+					let sum = 0;
+					
+					categoryListSlide.forEach(item => sum += item.offsetWidth);
+					
+					categoryNavigationNext.addEventListener('click', toNext);
+					categoryNavigationPrev.addEventListener('click', toPrev);
+					categoryDots.forEach(element => element.addEventListener('click', () => {}));
+	
+					categoryList.addEventListener('scroll', () => {
+						const { scrollLeft } = categoryList;
+						console.log('scroll-left', scrollLeft, 'offset-width', categoryContainerWidth);
+					});
+	
+					function toPrev() {
+						categoryList.scrollLeft -= 100;
+					}
+					function toNext() {
+						categoryList.scrollLeft += 100;
+						// if(sum > categoryContainerWidth) {}
+						// const ttt = 100 + 'px';
+						// document.querySelector('.slider-viewer').style.transform += 'translateX('+ttt+')';
+					}
+				};
+				carousel();
 				
-				// categoryListSlide.forEach(item => sum += item.offsetWidth);
 				
-				// categoryNavigationNext.addEventListener('click', toNext);
-				// categoryNavigationPrev.addEventListener('click', toPrev);
-				// categoryDots.forEach(element => element.addEventListener('click', () => {}));
+				const setDarkmode = () => {
+					const darkmodeButton = document.querySelector('.js-darkmode');
+					if(!darkmodeButton) return;
+					darkmodeButton.addEventListener('click', setDarkmode);
+	
+					function setDarkmode() {
+						document.body.classList.toggle('darkmode');
+						
+						const isDarkmode = document.body.classList.contains('darkmode');
+						if(isDarkmode) {
+							localStorage.setItem('theme', 'darkmode');
+						} else {
+							localStorage.removeItem('theme');
+							// localStorage.clear();
+						}
+					}
+					const lastTheme = localStorage.getItem('theme');
+					if(!lastTheme) return;
+					document.body.classList.add(lastTheme);
+				};
 
-				// categoryList.addEventListener('scroll', () => {
-				// 	const { scrollLeft } = categoryList;
-				// 	console.log('scroll-left', scrollLeft, 'offset-width', categoryContainerWidth);
-				// });
-
-				// function toPrev() {
-				// 	categoryList.scrollLeft -= 100;
-				// }
-				// function toNext() {
-				// 	categoryList.scrollLeft += 100;
-				// 	// if(sum > categoryContainerWidth) {}
-				// 	// const ttt = 100 + 'px';
-				// 	// document.querySelector('.slider-viewer').style.transform += 'translateX('+ttt+')';
-				// }
+				setDarkmode();
 				
 				customCursor();
 				checkAllcheckbox({checkAllElement: '.js-checkall', checkElements: '.js-check'});
