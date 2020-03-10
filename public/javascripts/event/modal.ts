8bef6399ea9dfebb3b0076d8dc7a8952792bcb22
soojubm <soojubm@gmail.com>
2020-03-10 11:33:25 +0900

    진행 상황

--- public/javascripts/event/modal.ts
+++ public/javascripts/event/modal.ts
@@ -22,15 +22,31 @@ const modal = ({ selector: trigger }: Parameter) => {
       pageY = window.pageYOffset
       
       showModal(nextElementSibling)
+
+      controlHistory()
+      window.onpopstate = history.onpushstate = function() {
+        if(window.location.href.split('/').pop().indexOf('modal') === -1) {
+          // 마지막 segment가 cards라면 모달이 아닌 리스트인 상태이어야한다.
+          closeModal(nextElementSibling); // 현재의 모달을 닫는다.
+        }
+      }
       
       document.addEventListener('click', () => closeModal(nextElementSibling))
       closeTrigger?.addEventListener('click', () => closeModal(nextElementSibling))
+
     })
   )
   
   const modalDialog = document.querySelectorAll('.modal-dialog')
   modalDialog.forEach(element => element.addEventListener('click', event => event.stopPropagation()))
 
+  function controlHistory() {
+    const state = { 'page_id': 1, 'user_id': 5 }
+    const title = ''
+    const url = 'hello-world.html'
+
+    history.pushState(state, title, url)
+  }
   function showModal(element) {
     element?.classList.add('is-visible')
     lockBody()
