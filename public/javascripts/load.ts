450b08cc07f56d2d53c3d942d1d3ac7d1f0ee9c7
soojubm <soojubm@gmail.com>
2020-04-13 20:38:26 +0900

    다운로드 스크립트

--- public/javascripts/load.ts
+++ public/javascripts/load.ts
@@ -20,6 +20,7 @@ export const detectBrowser = () => {
   const BrowserElement = document.querySelector<HTMLElement>('.js-browser')
   if (!BrowserElement) return
 
+  // const isMacBrowser = /Mac|iPod|iPhone|iPad/.test(navigator.platform);
   const userAgent = navigator.userAgent.toLowerCase()
 
   if (userAgent.indexOf('msie') > -1 || userAgent.indexOf('trident') > -1) {
@@ -28,6 +29,35 @@ export const detectBrowser = () => {
   } else if (userAgent.indexOf('chrome') !== -1) {
     console.log(userAgent, '크롬')
   }
+}
+
+const download = () => {
+  // <a href="/path/to/file" download>Download</a>
+  // Create a new link
+  const link = document.createElement('a');
+  link.download = 'file name';
+  link.href = '/path/to/file';
+
+  // Append to the document
+  document.body.appendChild(link);
+
+  // Trigger the click event
+  link.click();
+
+  // Remove the element
+  document.body.removeChild(link);
+  
+  //
+  // const data = JSON.stringify({ 'message': 'Hello Word' });
+  // const blob = new Blob([data], { type: 'application/json' });
+  // // Create new URL
+  // const url = window.URL.createObjectURL(blob);
+
+  // // Create a link and trigger the download
+  // ...
+
+  // // Free the URL created above
+  // window.URL.revokeObjectURL(url);
 }
 
 export const adjustTopPadding = () => {
