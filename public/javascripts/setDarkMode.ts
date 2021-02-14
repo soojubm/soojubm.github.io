4a12b1f61645113dd4e225f4ec47f0ec598c1402
soojubm <soojubm@gmail.com>
2021-02-14 15:35:49 +0900

    ff

--- public/javascripts/setDarkMode.ts
+++ public/javascripts/setDarkMode.ts
@@ -46,14 +46,14 @@ export const setDarkmode = () => {
 }
 
 function tt() {
-  document.body.classList.toggle('darkmode')
+  const DARK_CLASS = 'theme-dark'
+  document.body.classList.toggle(DARK_CLASS)
 
-  const isDarkmode = document.body.classList.contains('darkmode')
+  const isDarkmode = document.body.classList.contains(DARK_CLASS)
   if (isDarkmode) {
-    localStorage.setItem('theme', 'darkmode')
+    localStorage.setItem('theme', DARK_CLASS)
   } else {
     localStorage.removeItem('theme')
-    // localStorage.clear();
   }
 }
 
@@ -63,3 +63,13 @@ function loadTheme() {
 
   document.body.classList.add(lastTheme)
 }
+
+// function toggleTheme() {
+//   // 저장된 값이 없다면 시스템 설정을 기준으로 함
+//   const currentTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+//   const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
+
+//   // 최상위 엘리먼트에 설정, 로컬 스토리지에 설정을 저장
+//   document.documentElement.setAttribute('data-theme', newTheme)
+//   localStorage.setItem('theme', newTheme)
+// }
