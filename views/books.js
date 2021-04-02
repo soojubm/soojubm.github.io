73f50c069f4921c0452d03764e035ecb3694a20a
soojubm <soojubm@gmail.com>
2021-04-02 18:30:37 +0900

    refector: about

---
+++ views/books.js
@@ -0,0 +1,11 @@
+export const films = [
+  {
+    id: 1,
+    imgSrc: 'https://image.aladin.co.kr/product/25267/80/cover500/k422633201_1.jpg',
+    title: '임신중단에 대한 권리',
+    subTitle: '🦔:  정부의 ‘임신중단 부분 허용’ 발표에 고구마 먹은 듯 답답했다면!',
+    publisher: '오월의봄',
+    author: '박이대승',
+    publishedDate: '2020-01-02',
+  },
+];
