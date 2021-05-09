789089223018e0ce6d677ff7305b11577d6be6d3
soojubm <soojubm@gmail.com>
2021-05-09 21:25:53 +0900

    refector

--- public/javascripts/event/positionSticky.ts
+++ public/javascripts/event/positionSticky.ts
@@ -3,29 +3,34 @@
 // 언제 cancel
 
 import { throttle } from '../utils/optimizationUtils'
-import { getElementOffsetTop } from '../utils/elementUtils'
+import { getWindowScrollTop, getElementOffsetTop } from '../utils/elementUtils'
 
 type Parameters = {
   selector: string
   addClass: string
   isPassed: boolean
 }
-
+// todo renaming isPassed
 const positionSticky = ({ selector, addClass, isPassed }: Parameters) => {
   const element = document.querySelector<HTMLElement>(selector)
   if(!element) return
 
   const elementHeight = element.offsetHeight
-  const offsetTop = getElementOffsetTop(element)
+  let offsetTop = getElementOffsetTop(element)
   const offsetBottom = getElementOffsetTop(element) + elementHeight
 
   let previousScrollTop = isPassed ? offsetBottom : offsetTop
 
   window.addEventListener('scroll', throttle(handleElementScroll), false)
+  window.addEventListener('resize', throttle(setElementOffsetTop), false)
+
+  function setElementOffsetTop() {
+    offsetTop = getElementOffsetTop(element)
+  }
 
   function handleElementScroll() {
-    let currentScrollPostion = window.scrollY || window.pageYOffset
-    const isStuck = currentScrollPostion >= previousScrollTop
+    let currentScrollTop = getWindowScrollTop()
+    const isStuck = currentScrollTop >= previousScrollTop
     
     document.body.classList.toggle(addClass, isStuck)
     document.body.style.paddingTop = isStuck ? `${elementHeight}px` : '0'
--- public/javascripts/utils/elementUtils.ts
+++ public/javascripts/utils/elementUtils.ts
@@ -1,10 +1,13 @@
 export function getElementOffsetTop(element) {
   let offsetTop = element.offsetTop
   const hasParentElement = element.offsetParent
-
   if(hasParentElement) offsetTop += element.offsetParent.offsetTop
   
   return offsetTop
+}
+
+export function getWindowScrollTop() {
+  return window.scrollY || window.pageYOffset
 }
 
 export function getElementWidth(element) {
