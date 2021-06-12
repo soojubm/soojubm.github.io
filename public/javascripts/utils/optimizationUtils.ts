927cf289285e73ae3cc7a9ff84b4cab6bb83d6af
soojubm <soojubm@gmail.com>
2021-06-12 16:12:26 +0900

    ㅅ

--- public/javascripts/utils/optimizationUtils.ts
+++ public/javascripts/utils/optimizationUtils.ts
@@ -2,7 +2,33 @@ export function throttle(callback) {
   let timer: number | undefined
 
   return function() {
-    if(timer) window.cancelAnimationFrame(timer)
+    if (timer) window.cancelAnimationFrame(timer)
     timer = window.requestAnimationFrame(() => callback())
   }
 }
+
+export function temp() {
+  let resizeTimer
+  window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper')
+    clearTimeout(resizeTimer)
+
+    resizeTimer = setTimeout(() => {
+      document.body.classList.remove('resize-animation-stopper')
+    }, 400)
+  })
+}
+
+// let start = null
+// const element = document.querySelector('.js-navbar')
+
+// function step(timestamp) {
+//   if (!start) start = timestamp
+//   const progress = timestamp - start
+//   element.style.marginLeft = Math.min(progress / 10, 200) + 'px'
+
+//   if (progress >= 5000) return
+//   window.requestAnimationFrame(step)
+// }
+
+// window.requestAnimationFrame(step)
