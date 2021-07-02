7f82ae0e48d7552c3b2e4f00556acaf3265656cd
soojubm <soojubm@gmail.com>
2021-07-02 16:42:11 +0900

    test

--- public/javascripts/todo.js
+++ public/javascripts/todo.js
@@ -1,80 +1,69 @@
 
-	// const todo = document.querySelector('.js-todo');
-	// const todoList = document.querySelector('.js-todo-list');
-	// const todoListInput = document.querySelector('.js-todo-input');
-	// const todoSubmit = document.querySelector('.js-todo-submit');
-	// const todoClear = document.querySelector('.js-todo-clear');
-	// let todoItems =  [];
+// let todoItems =  [];
 
-	// function renderTodo(text) {
-	// 	const item = { id: todoItems.length + 1, text: text };
+function renderTodo(text) {
+  const item = { id: todoItems.length + 1, text: text }
 
-	// 	const li = document.createElement('li');
-	// 	const todoDelete = document.createElement('button');
-	// 	// todoDelete.classList.add('text-button');
-	// 	todoDelete.setAttribute('type','button');
-	// 	todoDelete.innerHTML = '<i class="icon-close" />';
+  const li = document.createElement('li')
+  const todoDelete = document.createElement('button')
+  // todoDelete.classList.add('text-button');
+  todoDelete.setAttribute('type', 'button')
+  todoDelete.innerHTML = '<i class="icon-close" />'
 
-	// 	const todoCheck = document.createElement('input');
-	// 	const todoCheckLabel = document.createElement('label');
-	// 	todoCheck.setAttribute('type', 'checkbox');
-	// 	todoCheck.setAttribute('id', item.id);
-	// 	todoCheckLabel.innerText = `${item.id} :: ${text}`;
-	// 	todoCheckLabel.setAttribute('for', item.id);
+  const todoCheck = document.createElement('input')
+  const todoCheckLabel = document.createElement('label')
+  todoCheck.setAttribute('type', 'checkbox')
+  todoCheck.setAttribute('id', item.id)
+  todoCheckLabel.innerText = `${item.id} :: ${text}`
+  todoCheckLabel.setAttribute('for', item.id)
 
-	// 	li.appendChild(todoDelete);
-	// 	li.appendChild(todoCheck);
-	// 	li.appendChild(todoCheckLabel);
+  li.appendChild(todoDelete)
+  li.appendChild(todoCheck)
+  li.appendChild(todoCheckLabel)
 
-	// 	todoList.appendChild(li);
-	// 	todoItems.push(item);
-	// 	// 체크박스 상태 저장안됨
+  todoList.appendChild(li)
+  todoItems.push(item)
+  // 체크박스 상태 저장안됨
 
-	// 	localStorage.setItem('todoItems', JSON.stringify(todoItems));
-	// 	todoListInput.value = '';
+  localStorage.setItem('todoItems', JSON.stringify(todoItems))
+  todoListInput.value = ''
 
-	// 	todoDelete.addEventListener('click', deleteTodo);
+  todoDelete.addEventListener('click', deleteTodo)
 
-	// 	function deleteTodo() {
-	// 		const target = event.target;
-	// 		target.parentNode.remove();
-	// 		const deletedItems = todoItems.filter(item => {
-	// 			return parseInt(target.nextElementSibling.id) !== item.id;
-	// 		});
-	// 		todoItems = deletedItems;
-			
-	// 		localStorage.setItem('todoItems', JSON.stringify(todoItems));
-	// 	}
-	// }
+  function deleteTodo() {
+    event.target.parentNode.remove()
+    const deletedItems = todoItems.filter(item => {
+      return parseInt(event.target.nextElementSibling.id) !== item.id
+    })
+    todoItems = deletedItems
 
-	// todoClear.addEventListener('click', () => {
-	// 	todoList.innerHTML = '';
-	// 	localStorage.removeItem('todoItems');
-	// });
+    localStorage.setItem('todoItems', JSON.stringify(todoItems))
+  }
+}
 
+todoClear.addEventListener('click', () => {
+  todoList.innerHTML = ''
+  localStorage.removeItem('todoItems')
+})
 
-	// todo.addEventListener('submit', event => {
-	// 	event.preventDefault();
-	// 	if(todoListInput.value.length < 1) return alert('입력해주세용');
-	// 	renderTodo(todoListInput.value);
-	// 	// todoList.innerHTML += `<li><input id=ab type=checkbox><label for=ab>${todoListInput.value}</label></li>`;
-	// });
+// todo.addEventListener('submit', event => {
+// 	event.preventDefault();
+// 	if(todoListInput.value.length < 1) return alert('입력해주세용');
+// 	renderTodo(todoListInput.value);
+// 	// todoList.innerHTML += `<li><input id=ab type=checkbox><label for=ab>${todoListInput.value}</label></li>`;
+// });
 
-	// const getLocalStorage = localStorage.getItem('todoListItems');
-	// if(getLocalStorage){
-	// 	const abc = JSON.parse(getLocalStorage);
-	// 	abc.forEach(function(item){
-	// 		renderTodo(item.text);
-	// 	});
-	// }
-
+// const getLocalStorage = localStorage.getItem('todoListItems');
+// if(getLocalStorage){
+// 	const abc = JSON.parse(getLocalStorage);
+// 	abc.forEach(function(item){
+// 		renderTodo(item.text);
+// 	});
+// }
 
 /*
 document.addEventListener('click', (event) => {
-	if(event.target.closest('.todo-control-remove')){
-		todoList.innerHTML = '';
-		localStorage.removeItem('todoListItems1');
-	}
+
 	const item = event.target.closest('.js-todo input');
 	if(item) {
 		if(item.checked) {
