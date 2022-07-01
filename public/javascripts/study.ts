6a19afc548eeab5324f1cf9a1703ab5f2d47f131
soojubm <soojubm@gmail.com>
2022-07-01 19:01:52 +0900

    test

--- public/javascripts/study.ts
+++ public/javascripts/study.ts
@@ -540,3 +540,124 @@ function hasClass(element, cls) {
 var pretty = document.createElement('script')
 pretty.src = 'https://cdn.rawgit.com/google/code-prettify/master/loader/' + 'run_prettify.js'
 document.body.appendChild(pretty)
+
+// !
+// How long you want the animation to take, in ms
+const animationDuration = 2000
+// Calculate how long each ‘frame’ should last if we want to update the animation 60 times per second
+const frameDuration = 1000 / 60
+// Use that to calculate how many frames we need to complete the animation
+const totalFrames = Math.round(animationDuration / frameDuration)
+// An ease-out function that slows the count as it progresses
+const easeOutQuad = t => t * (2 - t)
+
+// The animation function, which takes an Element
+const animateCountUp = el => {
+  let frame = 0
+  const countTo = parseInt(el.innerHTML, 10)
+  // Start the animation running 60 times per second
+  const counter = setInterval(() => {
+    frame++
+    // Calculate our progress as a value between 0 and 1
+    // Pass that value to our easing function to get our
+    // progress on a curve
+    const progress = easeOutQuad(frame / totalFrames)
+    // Use the progress value to calculate the current count
+    const currentCount = Math.round(countTo * progress)
+
+    // If the current count has changed, update the element
+    if (parseInt(el.innerHTML, 10) !== currentCount) {
+      el.innerHTML = currentCount
+    }
+
+    // If we’ve reached our last frame, stop the animation
+    if (frame === totalFrames) {
+      clearInterval(counter)
+    }
+  }, frameDuration)
+}
+
+// Run the animation on all elements with a class of ‘countup’
+const runAnimations = () => {
+  const countupEls = document.querySelectorAll('.countup')
+  countupEls.forEach(animateCountUp)
+}
+
+// ! star
+// <div id="star"></div>
+// <div id="display-star-value"></div>
+// body {
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-direction: column;
+//   height: 100vh;
+// }
+
+// .fa {
+//   font-size: 40px;
+// }
+
+// .fa-star-o:before {
+//   content: "\f006";
+//   color: #5f6368;
+// }
+
+// .fa-star:before {
+//   content: "\f005";
+//   color: #d56e0c;
+// }
+
+function Star(el, count, callback) {
+  let active = -1
+  const element = document.querySelector(el)
+  const fragment = document.createDocumentFragment()
+
+  for (let i = 1; i <= count; i++) {
+    const iElem = document.createElement('i')
+    iElem.classList.add('fa')
+    iElem.classList.add('fa-star-o')
+    iElem.dataset.ratingVal = i
+    fragment.appendChild(iElem)
+  }
+  element.appendChild(fragment)
+
+  element.addEventListener('mouseover', onMouseOver)
+  element.addEventListener('click', onClick)
+  element.addEventListener('mouseleave', onMouseLeave)
+
+  function onMouseOver(e) {
+    const ratingVal = e.target.dataset.ratingVal
+    if (!ratingVal) return
+
+    fill(ratingVal)
+  }
+
+  function fill(ratingVal) {
+    for (let i = 0; i < count; i++) {
+      if (i < ratingVal) {
+        element.children[i].classList.add('fa-star')
+      } else {
+        element.children[i].classList.remove('fa-star')
+      }
+
+      // element.children[i].classList.toggle('fa-star', i < ratingVal)
+    }
+  }
+
+  function onMouseLeave(e) {
+    fill(active)
+  }
+
+  function onClick(e) {
+    active = e.target.dataset.ratingVal
+    fill(active)
+    callback(active)
+  }
+}
+
+function getStar(value) {
+  document.getElementById('display-star-value').innerHTML = value
+}
+
+Star('#star', 5, getStar)
