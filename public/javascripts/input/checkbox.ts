c8c9c583d5aa45a62a5d3b40661d0568e18104e5
soojubm <soojubm@gmail.com>
2022-07-30 13:38:37 +0900

    test

--- public/javascripts/input/checkbox.ts
+++ public/javascripts/input/checkbox.ts
@@ -3,35 +3,38 @@ type Parameter = {
   checkSelector: string
 }
 
+// useSelection
+// useForm
+
 function checkbox({ checkAllSelector, checkSelector }: Parameter) {
   const checkAllElement = document.querySelector<HTMLInputElement>(checkAllSelector)
   const checkElements = document.querySelectorAll<HTMLInputElement>(checkSelector)
-
   if (!checkAllElement || !checkElements) return
 
   checkAllElement.addEventListener('change', () => checkAll(checkElements, checkAllElement))
   checkElements.forEach(checkItem => {
     checkItem.addEventListener('change', () => checkTarget(checkElements, checkAllElement))
   })
+}
 
-  function checkTarget(checkItems, checkAll) {
-    const checkboxElements: HTMLInputElement[] = Array.from(checkItems)
+function checkTarget(checkItems, checkAll) {
+  const checkboxElements: HTMLInputElement[] = Array.from(checkItems)
 
-    let isCheckedAll = checkboxElements.every(checkItem => checkItem.checked)
-    let isCheckedSome = checkboxElements.some(checkItem => checkItem.checked)
-    let isIndeterminate = isCheckedSome && !isCheckedAll
+  let isCheckedAll = checkboxElements.every(checkItem => checkItem.checked)
+  let isCheckedSome = checkboxElements.some(checkItem => checkItem.checked)
+  let isIndeterminate = isCheckedSome && !isCheckedAll
 
-    checkAll.checked = isCheckedAll
-    checkAll.indeterminate = isIndeterminate
-    checkAll.dataset.indeterminate = isIndeterminate
-  }
-  function checkAll(checkItems, checkAll) {
-    checkItems.forEach(checkItem => {
-      checkItem.checked = checkAll.checked
-      checkAll.indeterminate = false
-      checkAll.dataset.indeterminate = false
-    })
-  }
+  checkAll.checked = isCheckedAll
+  checkAll.indeterminate = isIndeterminate
+  checkAll.dataset.indeterminate = isIndeterminate
+}
+
+function checkAll(checkItems, checkAll) {
+  checkItems.forEach(checkItem => {
+    checkItem.checked = checkAll.checked
+    checkAll.indeterminate = false
+    checkAll.dataset.indeterminate = false
+  })
 }
 
 export default checkbox
