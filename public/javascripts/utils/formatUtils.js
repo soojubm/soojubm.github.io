34078164d7d4500dfeeb700345774375d0b97e5d
soojubm <soojubm@gmail.com>
2022-08-13 13:30:23 +0900

    test

--- public/javascripts/utils/formatUtils.js
+++ public/javascripts/utils/formatUtils.js
@@ -306,3 +306,11 @@ var sanitizeHTML = function(str) {
     return '&#' + c.charCodeAt(0) + ';'
   })
 }
+
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32
+}
