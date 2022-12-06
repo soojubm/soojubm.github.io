fb38083b86a0cf2fa6cc207e642e2c28e4ef4a4a
soojubm <30281891+soojubm@users.noreply.github.com>
2022-12-06 14:47:56 +0900

    test

--- public/javascripts/event/carousel.ts
+++ public/javascripts/event/carousel.ts
@@ -1,51 +1,88 @@
 const carousel = () => {
-  const category = document.querySelector<HTMLElement>('.carousel')
-  if (!category) return
+  const conatiner = document.querySelector<HTMLElement>('.js-carousel')
+  if (!conatiner) return
 
-  const categoryList = category.querySelector<HTMLElement>('.carousel-viewer')
-  const categoryListSlide = category.querySelectorAll<HTMLElement>('.carousel-viewer > button')
-  const categoryNavigationPrev = category.querySelector<HTMLElement>('.carousel-arrows-prev')
-  const categoryNavigationNext = category.querySelector<HTMLElement>('.carousel-arrows-next')
-  const categoryDots = category.querySelectorAll<HTMLElement>('.carousel-dots-dot')
+  const viewer = conatiner.querySelector<HTMLElement>('.js-carousel-viewer')
 
-  if (!categoryList || !categoryListSlide || !categoryNavigationPrev || !categoryNavigationNext || !categoryDots) return
+  const listItems = conatiner.querySelectorAll<HTMLElement>('.js-carousel-viewer > *')
 
-  const categoryContainerWidth = categoryList.offsetWidth
-  let sum = 0
+  const prevButton = conatiner.querySelector<HTMLElement>('.js-carousel-prev')
+  const nextButton = conatiner.querySelector<HTMLElement>('.js-carousel-next')
 
-  const isFirst = categoryList.scrollLeft === 0
-  if (isFirst) {
-    categoryNavigationPrev.style.display = 'none'
+  if (!viewer || !listItems || !prevButton || !nextButton) return
+
+  document.addEventListener('DOMContentLoaded', initialize)
+
+  nextButton.addEventListener('click', toNext)
+  prevButton.addEventListener('click', toPrev)
+
+  // const dots = conatiner.querySelectorAll<HTMLElement>('.carousel-dots-dot')
+  // if (!dots) return
+  // dots.forEach(element => element.addEventListener('click', () => {}))
+
+  viewer.addEventListener('scroll', scroll)
+
+  function initialize() {
+    const isFirst = viewer!.scrollLeft === 0
+    if (!isFirst) return
+
+    displayPrevButton('none')
+  }
+  function scroll() {
+    console.log(viewer?.scrollLeft)
+    const { scrollLeft } = viewer as HTMLElement
+
+    // scrollLeft 는 left 가 변경이 되는 것인지.
+
+    scrollLeft === 0 ? displayPrevButton('none') : displayPrevButton('flex')
+    // prevButton!.hidden = scrollLeft === 0
   }
 
-  categoryListSlide.forEach(item => (sum += item.offsetWidth))
-
-  categoryNavigationNext.addEventListener('click', toNext)
-  categoryNavigationPrev.addEventListener('click', toPrev)
-  categoryDots.forEach(element => element.addEventListener('click', () => {}))
-
-  categoryList.addEventListener('scroll', () => {
-    const { scrollLeft } = categoryList
-    console.log('scroll-left', scrollLeft, 'offset-width', categoryContainerWidth)
-
-    const isFirst = scrollLeft === 0
-    if (isFirst) {
-      categoryNavigationPrev.style.display = 'none'
-    } else {
-      categoryNavigationPrev.style.display = 'flex'
-    }
-    // categoryNavigationPrev.hidden = scrollLeft === 0
-  })
+  function displayPrevButton(value: 'flex' | 'none') {
+    prevButton!.style.display = value
+  }
 
   function toPrev() {
-    categoryList!.scrollLeft -= 100
+    viewer!.scrollLeft -= listItems[0].offsetWidth
   }
   function toNext() {
-    categoryList!.scrollLeft += 100
-    // if(sum > categoryContainerWidth) {}
-    // const ttt = 100 + 'px';
-    // document.querySelector('.carousel-viewer').style.transform += 'translateX('+ttt+')';
+    viewer!.scrollLeft += listItems[0].offsetWidth
   }
+
+  // let sum = 0
+  // listItems.forEach(item => (sum += item.offsetWidth))
+  // const categoryContainerWidth = viewer.offsetWidth
+  // if(sum > categoryContainerWidth) {}
+  // const ttt = 100 + 'px';
+  // document.querySelector('.carousel-viewer').style.transform += 'translateX('+ttt+')';
 }
 
 export default carousel
+
+// const slideWidth = slidesEl.offsetWidth;
+// const overallWidth = slidesEl.scrollWidth; // = slideWidth * numSlides
+
+// function navigate(direction) {
+//     const x = calculateNewPosition(direction);
+//     slidesEl.style.transform = `translateX(${x}px)`;
+// }
+
+// function calculateNewPosition(direction) {
+//     const str = slidesEl.style.transform;
+//     const x = str ? parseInt(str.match(/-?(\d+)/g)) : 0;
+
+//     if (direction === "forward") {
+//         const atLastSlide = x === -overallWidth + slideWidth;
+//         // Non-looping carousel: Stops at last slide
+//         return atLastSlide ? x : x - slideWidth;
+//         // Looping carousel: Transitions from last slide to first slide
+//         // return atLastSlide ? 0 : x-slideWidth;
+//     } else if (direction === "backward") {
+//         const atFirstSlide = x === 0;
+//         // Non-looping carousel: Stops at first slide
+//         return atFirstSlide ? x : x + slideWidth;
+//         // Looping carousel: Transitions from first slide to last slide
+//         // return atFirstSlide ? -overallWidth + slideWidth : x + slideWidth;
+//     }
+// }
+// })();
