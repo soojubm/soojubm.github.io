1652f3feb74737f31240de4eae807b7ace1f05bf
soojubm <30281891+soojubm@users.noreply.github.com>
2024-02-02 18:06:32 +0900

    test

--- pages/components/textfield/textfield.ts
+++ pages/components/textfield/textfield.ts
@@ -5,6 +5,7 @@ import main from './textfield.html'
 import '/pages/components/components.css'
 
 import '/public/stylesheets/shared.css'
+import '/public/stylesheets/components/form.css'
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('main')!.innerHTML = navbar + main + footer
