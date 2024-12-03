962d2680dae21305774c59fcc96e8549141e61b8
soojubm <soojubm@gmail.com>
2024-12-03 17:59:30 +0900

    Update

--- public/components/group/group.ts
+++ public/components/group/group.ts
@@ -21,17 +21,13 @@ class Group extends HTMLElement {
 
     // TODO radio = radiogroup role
 
-    // const align = 'vertical' || 'horizontal'
-    // TODO : alignment -> direction / align
-    const alignment = this.getAttribute('alignment')
-    container.dataset.alignment = alignment || ''
+    container.dataset.alignment = this.alignment || ''
 
     if (this.alignment) container.dataset.alignment = this.alignment
 
     const style = this.getAttribute('style')
     container.setAttribute('style', style || '')
 
-    if (this.bleeding) this.dataset.bleeding = 'true'
     if (this.variant) container.dataset.variant = this.variant
     // TODO : checkbox는 fieldset
 
@@ -41,10 +37,6 @@ class Group extends HTMLElement {
 
   get variant() {
     return this.getAttribute('variant')
-  }
-
-  get bleeding() {
-    return this.getAttribute('bleeding')
   }
 
   get alignment() {
