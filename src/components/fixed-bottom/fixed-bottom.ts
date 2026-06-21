31a535c16f00e199da39dfb52bd42acff70df207
soojubm <soojubm@gmail.com>
2026-06-21 21:21:26 +0900

    feat: add fixed bottom layout component

---
+++ src/components/fixed-bottom/fixed-bottom.ts
@@ -0,0 +1,35 @@
+import { LitElement, css, html } from 'lit'
+import { customElement } from 'lit/decorators.js'
+
+/**
+ * viewport 하단 고정만 담당하는 레이아웃 primitive.
+ * 높이, 간격, 배경과 내부 배치는 사용처에서 결정한다.
+ */
+@customElement('mm-fixed-bottom')
+export class FixedBottom extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+      width: 100%;
+      position: fixed;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: var(--zindex-fixed-bottom);
+    }
+  `
+
+  render() {
+    return html`
+      <slot></slot>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'mm-fixed-bottom': FixedBottom
+  }
+}
+
+export default FixedBottom
