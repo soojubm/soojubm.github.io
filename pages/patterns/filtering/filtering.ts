75e7c05aebfc97549153f02541fe386e43b8054e
soojubm <soojubm@gmail.com>
2026-07-22 19:30:54 +0900

    chore: 미사용 파일 정리
    sitemap에 연결되지 않은 페이지, 참조되지 않는 legacy 파일, 죽은 Hestia 웹폰트를 제거

--- pages/patterns/filtering/filtering.ts
+++
@@ -1,6 +0,0 @@
-import main from './index.html'
-import { renderLayout } from '../../../layouts/base-layouts'
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.innerHTML = renderLayout(main)
-})
--- pages/patterns/tabs/tabs.ts
+++
@@ -1,6 +0,0 @@
-import main from './index.html'
-import { renderDocumentLayout } from '../../../layouts/document-layout'
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.innerHTML = renderDocumentLayout(main)
-})
